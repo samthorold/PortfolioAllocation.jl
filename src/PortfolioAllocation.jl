16029eedbc395cd2057db39b272ce765118596f8
Sam Thorold <sam.thorold@gmail.com>
2022-08-11 20:04:17 +0100

    Minimum variance portfolio

--- src/PortfolioAllocation.jl
+++ src/PortfolioAllocation.jl
@@ -1,9 +1,39 @@
 module PortfolioAllocation
 
-function f(σ = 5.)
-    return σ * 2
+using LinearAlgebra
+
+"""
+    minimum_variance(σ, C)
+
+The asset weights giving the minimum variance portfolio.
+
+# Arguments
+- `σ::Array{Float64}`: the asset volatilities.
+- `C::Matrix{Float64}`: the asset correlation matrix.
+
+# Examples
+```jldoctest
+julia> using LinearAlgebra
+julia> σ = [.04, .06, .08, 1.];
+julia> C = fill(.2, length(σ), length(σ));
+julia> C[diagind(C)] .= 1.
+julia> minimum_variance(σ, C)
+4-element Vector{Float64}:
+ 0.6587633032466659
+ 0.22362926040684358
+ 0.08689209214603259
+ 0.030715344200458013
+```
+
+"""
+function minimum_variance(σ, C)
+    S = diagm(σ)
+    Σ = S * C * S
+    Σ_inv = inv(Σ)
+    i = ones(length(σ))
+    return (Σ_inv * i) / (transpose(i) * Σ_inv * i)
 end
 
-export f
+export minimum_variance
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,17 @@
-using PortfolioAllocation
+using LinearAlgebra
 using Test
+using PortfolioAllocation
 
-@testset "PortfolioAllocation.jl" begin
-    @test f(2.) == 4.
+@testset "Minimum variance" begin
+
+    σ = [.04, .06, .08, .10]
+    C = fill(.2, length(σ), length(σ))
+    C[diagind(C)] .= 1.
+    expected = [
+        0.6587633032466659
+        0.22362926040684358
+        0.08689209214603259
+        0.030715344200458013
+    ]
+    @test minimum_variance(σ, C) ≈ expected
 end
